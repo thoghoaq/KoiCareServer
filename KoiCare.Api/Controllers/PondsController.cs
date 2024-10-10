9351901e9ad63c59fdd4b35f8e9b4a753c9ddcab
HOANG Trung Thong <thoang5@mantu.com>
2024-10-10 13:36:12 +0700

    get all ponds

---
+++ KoiCare.Api/Controllers/PondsController.cs
@@ -0,0 +1,20 @@
+﻿using KoiCare.Application.Features.Pond;
+using KoiCare.Infrastructure.Middleware;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KoiCare.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PondsController(IMediator mediator) : BaseController
+    {
+        [Auth]
+        [HttpGet]
+        public async Task<ActionResult<GetListPonds.Result>> GetListPonds()
+        {
+            var result = await mediator.Send(new GetListPonds.Query());
+            return CommandResult(result);
+        }
+    }
+}
