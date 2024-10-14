d1342b5f2561151ff4772556d3129d14128f4305
Unknow <vuminhchau.12cxm@gmail.com>
2024-10-14 17:14:54 +0700

    Get Category + Product CRUD
    get duplicate id error in create product

---
+++ KoiCare.Api/Controllers/ProductController.cs
@@ -0,0 +1,70 @@
+﻿using KoiCare.Application.Features.Product;
+using KoiCare.Infrastructure.Middleware;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KoiCare.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductController(IMediator mediator) : BaseController
+    {
+        [HttpGet("get-all")]
+        public async Task<ActionResult<GetAllProduct.Result>> GetAllProducts([FromQuery] GetAllProduct.Query query)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await mediator.Send(query);
+            return CommandResult(result);
+        }
+
+        [Auth("Admin")]
+        [HttpPost("create")]
+        public async Task<ActionResult<CreateProduct.Result>> CreateProduct([FromBody] CreateProduct.Command command)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await mediator.Send(command);
+            return CommandResult(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<GetProductByID.Result>> GetProductByID(int id)
+        {
+            var query = new GetProductByID.Query { Id = id };
+            var result = await mediator.Send(query);
+            return CommandResult(result);
+        }
+
+        [Auth("Admin")]
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UpdateProduct.Result>> UpdateProduct(int id, [FromBody] UpdateProduct.Command command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest("ID mismatch");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await mediator.Send(command);
+            return CommandResult(result);
+        }
+
+        [Auth("Admin")]
+        [HttpDelete("{id}")]
+        public async Task<ActionResult<DeleteProduct.Result>> DeleteProduct(int id)
+        {
+            var command = new DeleteProduct.Command { Id = id };
+            var result = await mediator.Send(command);
+            return CommandResult(result);
+        }
+    }
+}
---
+++ KoiCare.Application/Features/Product/GetProductByID.cs
@@ -0,0 +1,61 @@
+﻿using KoiCare.Application.Abtractions.Database;
+using KoiCare.Application.Abtractions.Localization;
+using KoiCare.Application.Abtractions.LoggedUser;
+using KoiCare.Application.Commons;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace KoiCare.Application.Features.Product
+{
+    public class GetProductByID
+    {
+        public class Result
+        {
+            public int Id { get; set; }
+            public string Name { get; set; } = string.Empty;
+            public string Description { get; set; } = string.Empty;
+            public decimal Price { get; set; } = decimal.Zero;
+            public string ImageUrl { get; set; }
+            public string Category { get; set; }
+        }
+
+        public class Query : IRequest<CommandResult<Result>>
+        {
+            public int Id { get; set; }
+        }
+
+        public class Handler(
+            IAppLocalizer localizer,
+            ILogger<GetProductByID> logger,
+            ILoggedUser loggedUser,
+            IUnitOfWork unitOfWork,
+            IRepository<Domain.Entities.Product> productRepos
+            ) : BaseRequestHandler<Query, CommandResult<Result>>(localizer, logger, loggedUser, unitOfWork)
+        {
+            public override async Task<CommandResult<Result>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var product = await productRepos.Queryable()
+                    .Include(x => x.Category)
+                    .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+                if (product == null)
+                {
+                    return CommandResult<Result>.Fail("Product does not exist");
+                }
+
+                var result = new Result
+                {
+                    Id = product.Id,
+                    Name = product.Name,
+                    Description = product.Description,
+                    Price = product.Price,
+                    ImageUrl = product.ImageUrl,
+                    Category = product.Category.Name,
+                };
+
+                return CommandResult<Result>.Success(result);
+            }
+        }
+    }
+}
