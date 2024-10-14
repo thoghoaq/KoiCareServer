76d142ff47f16a18e82768259c84e86072cb6c28
Unknow <vuminhchau.12cxm@gmail.com>
2024-10-14 16:25:41 +0700

    Get all category and product crud

---
+++ KoiCare.Application/Features/Category/GetCategoryByID.cs
@@ -0,0 +1,53 @@
+﻿using KoiCare.Application.Abtractions.Database;
+using KoiCare.Application.Abtractions.Localization;
+using KoiCare.Application.Abtractions.LoggedUser;
+using KoiCare.Application.Commons;
+using KoiCare.Domain.Entities;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace KoiCare.Application.Features.Category
+{
+    public class GetCategoryByID
+    {
+        public class Result
+        {
+            public int Id { get; set; }
+            public string Name { get; set; } = string.Empty;
+        }
+        public class Query : IRequest<CommandResult<Result>>
+        {
+            public int Id { get; set; }
+        }
+
+        public class Handler(
+           IAppLocalizer localizer,
+           ILogger<GetCategoryByID> logger,
+           ILoggedUser loggedUser,
+           IUnitOfWork unitOfWork,
+           IRepository<Domain.Entities.Category> cateRepos
+           ) : BaseRequestHandler<Query, CommandResult<Result>>(localizer, logger, loggedUser, unitOfWork)
+        {
+            public override async Task<CommandResult<Result>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var category = await cateRepos.Queryable()
+                    .Include(x => x.Products)
+                    .FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+
+                if (category == null)
+                {
+                    return CommandResult<Result>.Fail("Category không tồn tại");
+                }
+
+                var result = new Result
+                {
+                    Id = category.Id,
+                    Name = category.Name
+                };
+
+                return CommandResult<Result>.Success(result);
+            }
+        }
+    }
+}
---
+++ KoiCare.Application/Features/Product/UpdateProduct.cs
@@ -0,0 +1,70 @@
+﻿using KoiCare.Application.Abtractions.Database;
+using KoiCare.Application.Abtractions.Localization;
+using KoiCare.Application.Abtractions.LoggedUser;
+using KoiCare.Application.Commons;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+using System.Net;
+
+namespace KoiCare.Application.Features.Product
+{
+    public class UpdateProduct
+    {
+        public class Result
+        {
+            public string? Message { get; set; }
+        }
+
+        public class Command : IRequest<CommandResult<Result>>
+        {
+            public required int Id { get; set; }
+            public required string Name { get; set; }
+            public required string Description { get; set; }
+            public required decimal Price { get; set; }
+            public required string ImageUrl { get; set; }
+            public required int CategoryId { get; set; }
+        }
+
+        public class Handler(
+            IAppLocalizer localizer,
+            ILogger<UpdateProduct> logger,
+            ILoggedUser loggedUser,
+            IUnitOfWork unitOfWork,
+            IRepository<Domain.Entities.Product> productRepos
+            ) : BaseRequestHandler<Command, CommandResult<Result>>(localizer, logger, loggedUser, unitOfWork)
+        {
+            public override async Task<CommandResult<Result>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                try
+                {
+                    //Tìm Product trong cơ sở dữ liệu
+                    var product = await productRepos.Queryable().FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+                    if (product == null)
+                    {
+                        return CommandResult<Result>.Fail(_localizer["Product not found"]);
+                    }
+                    //Update Product
+                    product.Name = request.Name;
+                    product.Description = request.Description;
+                    product.Price = request.Price;
+                    product.ImageUrl = request.ImageUrl;
+                    product.CategoryId = request.CategoryId;
+
+                    productRepos.Update(product);
+                    await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+                    return CommandResult<Result>.Success(new Result
+                    {
+                        Message = _localizer["Product updated successfully"]
+                    });
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, ex.Message);
+                    return CommandResult<Result>.Fail(HttpStatusCode.InternalServerError, ex.Message);
+                }
+            }
+        }
+    }
+}
