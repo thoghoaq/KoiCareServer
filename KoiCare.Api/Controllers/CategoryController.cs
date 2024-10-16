ca4bffeb10e9512564072d70ea81bfa943c96052
Unknow <vuminhchau.12cxm@gmail.com>
2024-10-16 23:26:04 +0700

    Get all Category and Product CRUD

---
+++ KoiCare.Api/Controllers/CategoryController.cs
@@ -0,0 +1,31 @@
+﻿using KoiCare.Application.Features.Category;
+using KoiCare.Infrastructure.Middleware;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KoiCare.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoryController(IMediator mediator) : BaseController
+    {
+        [HttpGet("get-all")]
+        public async Task<ActionResult<GetAllCategory.Result>> GetAllBlogs([FromQuery] GetAllCategory.Query query)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await mediator.Send(query);
+            return CommandResult(result);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<GetCategoryByID.Result>> GetCategoryById(int id)
+        {
+            var query = new GetCategoryByID.Query { Id = id };
+            var result = await mediator.Send(query);
+            return CommandResult(result);
+        }
+    }
+}
---
+++ KoiCare.Application/Features/Product/GetAllProduct.cs
@@ -0,0 +1,73 @@
+﻿using KoiCare.Application.Abtractions.Database;
+using KoiCare.Application.Abtractions.Localization;
+using KoiCare.Application.Abtractions.LoggedUser;
+using KoiCare.Application.Commons;
+using KoiCare.Application.Features.Category;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace KoiCare.Application.Features.Product
+{
+    public class GetAllProduct
+    {
+        public record ProductResult
+        {
+            public int Id { get; set; }
+            public required string Name { get; set; }
+            public string? Description { get; set; }
+            public decimal Price { get; set; }
+            public string? ImageUrl { get; set; }
+            public required string Category { get; set; }
+        }
+
+        public class Result
+        {
+            public List<ProductResult> Products { get; set; } = new List<ProductResult>();
+        }
+        public class Query : IRequest<CommandResult<Result>>
+        {
+            public int? PageNumber { get; set; }
+            public int? PageSize { get; set; }
+            public string? Search { get; set; }
+            public int? CategoryId { get; set; }
+        }
+
+        public class Handler(
+            IAppLocalizer localizer,
+            ILogger<GetAllCategory> logger,
+            ILoggedUser loggedUser,
+            IUnitOfWork unitOfWork,
+            IRepository<Domain.Entities.Product> productRepos
+            ) : BaseRequestHandler<Query, CommandResult<Result>>(localizer, logger, loggedUser, unitOfWork)
+        {
+            public override Task<CommandResult<Result>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var query = productRepos.Queryable()
+                    .Include(x => x.Category) // Giả sử có liên kết với bảng Category
+                    .Where(x => request.Search == null || x.Name.Contains(request.Search!))
+                    .Where(x => x.CategoryId.Equals(request.CategoryId))
+                    .Select(x => new ProductResult
+                    {
+                        Id = x.Id,
+                        Name = x.Name,
+                        Description = x.Description,
+                        Price = x.Price,
+                        ImageUrl = x.ImageUrl,
+                        Category = x.Category.Name
+                    });
+                // Phân trang
+                if (request.PageNumber.HasValue && request.PageSize.HasValue)
+                {
+                    query = query.Skip((request.PageNumber.Value - 1) * request.PageSize.Value).Take(request.PageSize.Value);
+                }
+
+                // Trả về kết quả
+                return Task.FromResult(CommandResult<Result>.Success(new Result
+                {
+                    Products = query.ToList()
+                }));
+            }
+        }
+    }
+}
