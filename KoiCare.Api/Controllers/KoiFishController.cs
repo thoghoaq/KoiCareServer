9c60c965b818f88f9f7bbc0e7bf3c5dd3027d137
Unknow <102649750+OmegaCode404@users.noreply.github.com>
2024-10-23 17:32:49 +0700

    Manage koi fish (#18)
    * Get all Category and Product CRUD
    
    * Get orders api
    
    * Manage koi fish

---
+++ KoiCare.Api/Controllers/KoiFishController.cs
@@ -0,0 +1,64 @@
+﻿using KoiCare.Application.Features.Koifish;
+using KoiCare.Application.Features.Product;
+using KoiCare.Infrastructure.Middleware;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KoiCare.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class KoiFishController(IMediator mediator) : BaseController
+    {
+
+        [HttpGet("get-all-koi-types")]
+        public async Task<ActionResult<GetAllKoiType.Result>> GetAllKoiTypes([FromQuery] GetAllKoiType.Query query)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await mediator.Send(query);
+            return CommandResult(result);
+        }
+
+        [Auth]
+        [HttpGet("{PondId}")]
+        public async Task<ActionResult<GetAllFishInPond.Result>> GetAllFishInPond(int PondId)
+        {
+            var query = new GetAllFishInPond.Query { PondId = PondId };
+            var result = await mediator.Send(query);
+            return CommandResult(result);
+        }
+
+        [Auth("User")]
+        [HttpPost("create")]
+        public async Task<ActionResult<CreateKoiFish.Result>> CreateKoiFish([FromBody] CreateKoiFish.Command command)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await mediator.Send(command);
+            return CommandResult(result);
+        }
+
+        [Auth("User")]
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UpdateKoiFish.Result>> UpdateKoiFish(int id, [FromBody] UpdateKoiFish.Command command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest("ID mismatch");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await mediator.Send(command);
+            return CommandResult(result);
+        }
+    }
+}
---
+++ KoiCare.Application/Features/Koifish/CreateKoiFish.cs
@@ -0,0 +1,86 @@
+﻿using KoiCare.Application.Abtractions.Database;
+using KoiCare.Application.Abtractions.Localization;
+using KoiCare.Application.Abtractions.LoggedUser;
+using KoiCare.Application.Commons;
+using KoiCare.Application.Features.Product;
+using MediatR;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KoiCare.Application.Features.Koifish
+{
+    public class CreateKoiFish
+    {
+        public class Command : IRequest<CommandResult<Result>>
+        {
+            public required string Name { get; set; }
+            public required int KoiTypeId { get; set; }
+            public required int PondId { get; set; }
+            public string? ImageUrl { get; set; }
+            public decimal Age { get; set; }
+            public decimal? Weight { get; set; }
+            public int? Gender { get; set; }
+            public int? Origin { get; set; }
+            public int? Shape { get; set; }
+            public decimal? Breed { get; set; }
+            public decimal? Length { get; set; }
+            //public required int CategoryId { get; set; }
+        }
+
+        public class Result
+        {
+            public string? Message { get; set; }
+        }
+
+        public class Handler(
+           IRepository<Domain.Entities.KoiIndividual> koiRepos,
+           IAppLocalizer localizer,
+           ILogger<CreateKoiFish> logger,
+           ILoggedUser loggedUser,
+           IUnitOfWork unitOfWork
+        ) : BaseRequestHandler<Command, CommandResult<Result>>(localizer, logger, loggedUser, unitOfWork)
+        {
+            private readonly IRepository<Domain.Entities.KoiIndividual> _koiRepos = koiRepos;
+
+            public override async Task<CommandResult<Result>> Handle(Command request, CancellationToken cancellationToken)
+            {
+                try
+                {
+                    var koifish = new Domain.Entities.KoiIndividual()
+                    {
+                        Name = request.Name,
+                        KoiTypeId = request.KoiTypeId,
+                        PondId = request.PondId,
+                        ImageUrl = request.ImageUrl,
+                        Age = request.Age,
+                        Weight = request.Weight,
+                        Gender = request.Gender,
+                        Origin = request.Origin,
+                        Shape = request.Shape,
+                        Length = request.Length,
+                        Breed = request.Breed,
+                        //CategoryId = request.CategoryId
+                    };
+                    
+                    await _koiRepos.AddAsync(koifish, cancellationToken);
+                    await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+                    return CommandResult<Result>.Success(new Result
+                    {
+                        Message = _localizer["Koi fish added successfully"],
+                    });
+                }
+                catch (Exception ex)
+                {
+                    _logger.LogError(ex, ex.Message);
+                    return CommandResult<Result>.Fail(HttpStatusCode.InternalServerError, ex.Message);
+                }
+            }
+        }
+    }
+}
