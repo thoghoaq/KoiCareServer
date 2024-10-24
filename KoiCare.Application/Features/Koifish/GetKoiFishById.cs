1b7ff64d234997d79e10c60704d4bd4395621104
Unknow <102649750+OmegaCode404@users.noreply.github.com>
2024-10-24 23:32:44 +0700

    update datatype (#20)

--- KoiCare.Application/Features/Koifish/GetKoiFishById.cs
+++ KoiCare.Application/Features/Koifish/GetKoiFishById.cs
@@ -23,9 +23,9 @@ namespace KoiCare.Application.Features.Koifish
             public decimal? Length { get; set; }
             public decimal? Weight { get; set; }
             public int? Gender { get; set; }
-            public int? Origin { get; set; }
+            public string? Origin { get; set; }
             public int? Shape { get; set; }
-            public decimal? Breed { get; set; }
+            public string? Breed { get; set; }
         }
 
         public class Query : IRequest<CommandResult<Result>>
