7125082e408816e93475019d2b5a432f305b47c9
HOANG Trung Thong <thoang5@mantu.com>
2024-10-24 21:44:58 +0700

    create feeding

---
+++ KoiCare.Api/Controllers/FeedingController.cs
@@ -0,0 +1,65 @@
+﻿using KoiCare.Application.Features.Feeding;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace KoiCare.Api.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class FeedingController(IMediator mediator) : BaseController
+    {
+        [HttpGet("{KoiIndividualId}")]
+        public async Task<ActionResult<GetFeedingSchedule.Result>> GetFeedingSchedule(int KoiIndividualId)
+        {
+            var query = new GetFeedingSchedule.Query { KoiIndividualId = KoiIndividualId };
+            var result = await mediator.Send(query);
+            return CommandResult(result);
+        }
+
+        [HttpPost("create")]
+        public async Task<ActionResult<CreateFeedingSchedule.Result>> CreateFeedingSchedule([FromBody] CreateFeedingSchedule.Command command)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await mediator.Send(command);
+            return CommandResult(result);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult<UpdateFeedingSchedule.Result>> UpdateFeedingSchedule(int id, [FromBody] UpdateFeedingSchedule.Command command)
+        {
+            if (id != command.Id)
+            {
+                return BadRequest("ID mismatch");
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var result = await mediator.Send(command);
+            return CommandResult(result);
+        }
+
+        [HttpPost("feed-calculation")]
+        public async Task<ActionResult<CalculateFeedingAmount.Result>> CalculateFeedingAmount([FromBody] CalculateFeedingAmount.Command command)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+            var result = await mediator.Send(command);
+            return CommandResult(result);
+        }
+
+        [HttpGet("feed-calculation")]
+        public async Task<ActionResult<GetFeedingCalculation.QueryResult>> CalculateFeedingAmount([FromQuery] GetFeedingCalculation.Query query)
+        {
+            var result = await mediator.Send(query);
+            return CommandResult(result);
+        }
+    }
+}
---
+++ KoiCare.Application/Features/Feeding/GetFeedingCalculation.cs
@@ -0,0 +1,55 @@
+﻿using KoiCare.Application.Abtractions.Database;
+using KoiCare.Application.Abtractions.Localization;
+using KoiCare.Application.Abtractions.LoggedUser;
+using KoiCare.Application.Commons;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging;
+
+namespace KoiCare.Application.Features.Feeding
+{
+    public class GetFeedingCalculation
+    {
+        public class Query : IRequest<CommandResult<QueryResult>>
+        {
+            public int KoiIndividualId { get; set; }
+        }
+
+        public class QueryResult
+        {
+            public int Id { get; set; }
+            public decimal Amount { get; set; }
+            public decimal DailyAmount { get; set; }
+            public decimal Frequency { get; set; }
+            public int Type { get; set; }
+        }
+
+        public class Handler(
+        IAppLocalizer localizer,
+        ILogger<CalculateFeedingAmount> logger,
+        ILoggedUser loggedUser,
+        IUnitOfWork unitOfWork,
+        IRepository<Domain.Entities.FeedCalculation> feedCalculationRepos
+        ) : BaseRequestHandler<Query, CommandResult<QueryResult>>(localizer, logger, loggedUser, unitOfWork)
+        {
+            public override async Task<CommandResult<QueryResult>> Handle(Query request, CancellationToken cancellationToken)
+            {
+                var feedCalculation = await feedCalculationRepos.Queryable().FirstOrDefaultAsync(x => x.KoiIndividualId == request.KoiIndividualId, cancellationToken);
+
+                if (feedCalculation == null)
+                {
+                    return CommandResult<QueryResult>.Fail(localizer["Feeding calculation not found"]);
+                }
+
+                return CommandResult<QueryResult>.Success(new QueryResult
+                {
+                    Id = feedCalculation.Id,
+                    Amount = feedCalculation.DailyAmount * feedCalculation.Frequency,
+                    DailyAmount = feedCalculation.DailyAmount,
+                    Frequency = feedCalculation.Frequency,
+                    Type = feedCalculation.Type
+                });
+            }
+        }
+    }
+}
